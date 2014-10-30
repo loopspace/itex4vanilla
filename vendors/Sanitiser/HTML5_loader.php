316cecc7b137d879c2d3e1ebd7a01681f1696ee8
Andrew Stacey <stacey@math.ntnu.no>
2014-10-30 17:57:13 +0000

    Modified the sanitiser to work with html5-php and added a loader file.

---
+++ vendors/Sanitiser/HTML5_loader.php
@@ -0,0 +1,24 @@
+<?php
+
+require "../Masterminds/HTML5.php";
+require "../Masterminds/HTML5/Elements.php";
+require "../Masterminds/HTML5/Entities.php";
+require "../Masterminds/HTML5/Exception.php";
+require "../Masterminds/HTML5/InstructionProcessor.php";
+require "../Masterminds/HTML5/Parser/CharacterReference.php";
+require "../Masterminds/HTML5/Parser/EventHandler.php";
+require "../Masterminds/HTML5/Parser/DOMTreeBuilder.php";
+require "../Masterminds/HTML5/Parser/InputStream.php";
+require "../Masterminds/HTML5/Parser/StringInputStream.php";
+require "../Masterminds/HTML5/Parser/FileInputStream.php";
+require "../Masterminds/HTML5/Parser/ParseError.php";
+require "../Masterminds/HTML5/Parser/Scanner.php";
+require "../Masterminds/HTML5/Parser/Tokenizer.php";
+require "../Masterminds/HTML5/Parser/TreeBuildingRules.php";
+require "../Masterminds/HTML5/Parser/UTF8Utils.php";
+require "../Masterminds/HTML5/Serializer/HTML5Entities.php";
+require "../Masterminds/HTML5/Serializer/RulesInterface.php";
+require "../Masterminds/HTML5/Serializer/OutputRules.php";
+require "../Masterminds/HTML5/Serializer/Traverser.php";
+
+?>
